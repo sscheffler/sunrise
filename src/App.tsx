17b387168cb2f3913fbdeced89996a7fbe85fbde
stefanscheffler <sscheffler@avantgarde-labs.de>
2023-10-31 18:46:18 +0100

    add form and render items

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import {useState} from 'react';
+
 import Logo from './components/Logo';
 import Sunrise from './components/Sunrise';
+import List from './components/List';
+import {v4 as uuidv4} from 'uuid';
 
+export type TodoItem = {  
+  id: string;
+  itemText: string;
+};
 
 const App = () => {
+  const [newTodoItem, setNewTodoItem] = useState<string>('');
+  const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { 
+    e.preventDefault(); 
+    const newItems = {
+      id: uuidv4(),
+      itemText: newTodoItem,
+    }
+    setTodoItems([newItems, ...todoItems]);
+    setNewTodoItem('');
+  }
+
   return (
     <>
     <main>
       <Logo />
       <Sunrise />
+        <form className='flex items-center justify-center mt-10 lg:mt-20' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Enter your todo item' 
+            className='py-2 px-5 rounded-lg bg-gray-700 text-white tracking-wide'
+            autoComplete='off'
+            value={newTodoItem}
+            onChange={(e) => setNewTodoItem(e.target.value)}
+            />
+        </form>
+        <List todoItems={todoItems}/>
     </main>
     </>
   );
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,28 @@
-const List = () => {
+import { TodoItem } from "../App"
+import {IoClose} from 'react-icons/io5';
+
+interface ListProps {
+  todoItems:TodoItem[];
+}
+
+const List = ({todoItems}: ListProps) => {
   return (
     <div>
-      <h2 className="font-bold text-4xl">List Component</h2>
+      <article>
+        <ul className="bg-gray-700 mx-5 rounded-lg mt-10">
+          {todoItems.map(({id, itemText}) => (
+            <ul className="todo-list px-5 flex items-center justify-between border-b border-gray-600">
+              <li key={id} className=" text-white py-3 tracking-wider">
+                {itemText}
+              </li>
+              <button className="text-xl">
+                <IoClose className="text-red-400"/>
+              </button>
+            </ul>
+            
+          ))}
+        </ul>
+      </article>
     </div>
   )
 }   
