9e5129f5ec36f0c39024adfc9a58f442fc9e9258
stefanscheffler <sscheffler@avantgarde-labs.de>
2023-10-31 10:20:34 +0100

    tailwind grid test

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
+    <div>
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <div className="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <div>07</div>
+        <div>08</div>
+        <div>09</div>
+      </div>
     </div>
+
   );
 }
 
